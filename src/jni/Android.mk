1c15ee3ef572e25e9fc601661fbb0cdc771679ab
XaView <79099247+XaViewDnK@users.noreply.github.com>
2026-05-17 20:56:43 +0300

    undo

--- src/jni/Android.mk
+++ src/jni/Android.mk
@@ -4,9 +4,4 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := DamnWrapper32
 LOCAL_SRC_FILES := main.cpp dylb.cpp sqlite3.c
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lz
-
-# Принудительная компиляция в полноценном 32-битном режиме ARM (вместо сжатого Thumb-2).
-# Это критически важно для старого кода iOS, использующего ассемблерные вставки или специфичную математику.
-LOCAL_ARM_MODE := arm
-
 include $(BUILD_SHARED_LIBRARY)
--- src/jni/Application.mk
+++ src/jni/Application.mk
@@ -1,10 +1,3 @@
 APP_ABI := armeabi-v7a
 APP_PLATFORM := android-21
 APP_STL := c++_static
-
-# --- НОВЫЕ ПАРАМЕТРЫ ---
-# Флаги совместимости для старого кода:
-# -fno-strict-aliasing: отключает агрессивную оптимизацию указателей (спасает от крашей из-за нарушения стандартов C/C++)
-# -fwrapv: разрешает безопасное переполнение знаковых целых чисел (часто использовалось в ретро-играх)
-APP_CFLAGS += -fno-strict-aliasing -fwrapv
-APP_CPPFLAGS += -fno-strict-aliasing -fwrapv
